ca486ffd51782e91e741ccf80fcab35a7d400103
sbadhurunfarhana <47767032+sbadhurunfarhana@users.noreply.github.com>
2019-02-19 14:09:59 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,10 @@
+a=str(raw_input())
+if (a.isalpha()):
+    if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u"):
+        print("vowel")
+    else:
+        print("consonant")
+else:
+    print("invalid")
+
+    
